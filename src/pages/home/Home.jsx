7fee6bff849b77bc2df09cb86990cc0142e3177c
Herby <herbyosbourne31@gmail.com>
2022-07-09 21:45:39 -0500

    added Featured and Chart component to Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Outlet, useOutlet } from 'react-router-dom';
+import Chart from '../../components/chart/Chart';
 import Navbar from '../../components/navbar/Navbar';
 import Widgets from '../../components/widget/Widgets';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Featured from '../../components/featured/Featured';
 
 import './home.scss';
 
@@ -13,7 +15,15 @@ const Home = () => {
       <Sidebar />
       <div className="home-container">
         <Navbar />
-        {outlet || <Widgets />}
+        {outlet || (
+          <>
+            <Widgets />{' '}
+            <div className="charts">
+              <Featured />
+              <Chart />
+            </div>
+          </>
+        )}
         {/* <Outlet /> */}
       </div>
     </div>
