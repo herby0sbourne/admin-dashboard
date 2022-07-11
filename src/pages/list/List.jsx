22b9cc74e3921ba24b1853bc6377331e4618e5bd
Herby <herbyosbourne31@gmail.com>
2022-07-11 07:54:22 -0500

    import DataTable into List page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import DataTable from '../../components/data-table/DataTable';
+import './list.scss';
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list-table">
+      <div className="list-container">
+        <DataTable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
