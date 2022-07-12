9ce241a155d7a52df1e2a1c2630b17d3478186d7
Herby <herbyosbourne31@gmail.com>
2022-07-12 14:09:12 -0500

    passed userInputs  and productInputs as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import New from './pages/new/New';
+import List from './pages/list/List';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import List from './pages/list/List';
 import Single from './pages/single/Single';
 
 import './App.css';
+import { userInputs, productInputs } from './formfieldData';
 
 function App() {
   return (
@@ -15,12 +17,18 @@ function App() {
           <Route path="users">
             <Route index element={<List />} />
             <Route path=":userId" element={<Single />} />
-            {/* <Route path='new' element={<New />} /> */}
+            <Route
+              path="new"
+              element={<New inputs={userInputs} title="Add new User" />}
+            />
           </Route>
           <Route path="products">
             <Route index element={<List />} />
             <Route path=":productId" element={<Single />} />
-            {/* <Route path='new' element={<New />} /> */}
+            <Route
+              path="new"
+              element={<New inputs={productInputs} title="Add new Product" />}
+            />
           </Route>
           {/* <Route path="dashboard" element={<Test />}>
             <Route path="messages" element={<DashboardMessages />} />
