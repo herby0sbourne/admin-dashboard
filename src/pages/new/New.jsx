dba69e642888be172fed627a6b8524c11b69242e
Herby <herbyosbourne31@gmail.com>
2022-07-12 14:13:07 -0500

    created new component for added new products or users

---
+++ src/pages/new/New.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
+
+import './new.scss';
+import FormInput from '../../components/from-input/FormInput';
+
+const New = ({ title, inputs }) => {
+  const [file, setFile] = useState('');
+  console.log(file);
+  return (
+    <div>
+      <div className="new">
+        <div className="new-container">
+          <div className="top">
+            <h1>{title}</h1>
+          </div>
+          <div className="bottom">
+            <div className="left">
+              <img
+                src={
+                  file
+                    ? URL.createObjectURL(file)
+                    : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+                }
+                alt="product"
+              />
+            </div>
+            <div className="right">
+              <form>
+                <div className="form-input">
+                  <label htmlFor="image">
+                    Upload Image:
+                    <DriveFolderUploadOutlinedIcon className="icon" />
+                  </label>
+                  <input
+                    type="file"
+                    id="image"
+                    onChange={(e) => {
+                      setFile(e.target.files[0]);
+                    }}
+                    style={{ display: 'none' }}
+                  />
+                </div>
+                {inputs.map(({ id, ...input }) => {
+                  return <FormInput key={id} {...input} />;
+                })}
+                {/* <FormInput
+                  forId="username"
+                  label="Username"
+                  type="text"
+                  placeholder="John_doe eat"
+                />
+                <FormInput
+                  forId="fullname"
+                  label="Name and Surname"
+                  type="text"
+                  placeholder="John Doe"
+                /> */}
+                <button>Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default New;
