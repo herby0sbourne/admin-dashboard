e3928a86fdd6eea2ddc2a282d61e3c02767fb1ae
Herby <herbyosbourne31@gmail.com>
2022-07-13 08:40:16 -0500

    added Link path and useState to hold userdata

--- src/components/data-table/DataTable.jsx
+++ src/components/data-table/DataTable.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { DataGrid } from '@mui/x-data-grid';
 
 import './data-table.scss';
 import { userColumns, userRows } from './tableData';
 
 const DataTable = () => {
+  const { pathname } = useLocation();
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
   const actionCell = [
     {
       field: 'action',
       headerName: 'Action',
       width: 200,
-      renderCell: () => {
+      renderCell: (params) => {
         return (
           <div className="cell-action">
-            <div className="view-button">View</div>
-            <div className="delete-button">Delete</div>
+            <Link to={'/users/test'}>
+              <div className="view-button">View</div>
+            </Link>
+            <div className="delete-button" onClick={() => handleDelete(params.row.id)}>
+              Delete
+            </div>
           </div>
         );
       },
     },
   ];
+
   return (
     <div className="data-table">
+      <div className="data-title">
+        Add New User
+        <Link to={`${pathname}/new`} className="link">
+          Add New
+        </Link>
+      </div>
       <DataGrid
-        rows={userRows}
+        className="data-grid"
+        rows={data}
         columns={userColumns.concat(actionCell)}
         pageSize={8}
         rowsPerPageOptions={[8]}
