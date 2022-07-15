a77c1b2e7340689ae21e54f96ffe9624ecb00b54
Herby <herbyosbourne31@gmail.com>
2022-07-15 11:54:47 -0500

    created login component

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
+import './login.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [error, setError] = useState(true);
+  const [user, setUser] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, user.email, user.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        navigate('/');
+      })
+      .catch((error) => {
+        console.log(error.code);
+        console.log(error.message);
+        setError(true);
+      });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="email" onChange={handleChange} />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password</span>}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
