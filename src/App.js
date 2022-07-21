e776bdadb3ffd882fce77de70be8eb3126c9ed70
Herby <herbyosbourne31@gmail.com>
2022-07-20 19:10:27 -0500

    updated Route to use RequiredAuth for protection

--- src/App.js
+++ src/App.js
@@ -1,85 +1,49 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import New from './pages/new/New';
 import List from './pages/list/List';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Single from './pages/single/Single';
+import { AuthContext } from './context/AuthContext';
 
 // import './App.css';
 import './theme/dark.scss';
 import { userInputs, productInputs } from './data/formfieldData';
+// import { auth } from './firebase/firebase';
+import RequireAuth from './components/auth/RequireAuth';
 
 function App() {
-  const currentUser = false;
+  // const { userAuth } = useContext(AuthContext);
 
-  const RequireAuth = ({ children }) => {
-    return currentUser ? children : <Navigate to={'/login'} replace={true} />;
-  };
+  // console.log(userAuth.currentUser);
 
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
+          {/* Public Routes */}
           <Route path="login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <RequireAuth>
-                <Home />
-              </RequireAuth>
-            }
-          >
-            <Route path="users">
-              <Route
-                index
-                element={
-                  <RequireAuth>
-                    <List />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path=":userId"
-                element={
-                  <RequireAuth>
-                    <Single />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path="new"
-                element={
-                  <RequireAuth>
-                    <New inputs={userInputs} title="Add new User" />
-                  </RequireAuth>
-                }
-              />
-            </Route>
-            <Route path="products">
-              <Route
-                index
-                element={
-                  <RequireAuth>
-                    <List />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path=":productId"
-                element={
-                  <RequireAuth>
-                    <Single />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path="new"
-                element={
-                  <RequireAuth>
-                    <New inputs={productInputs} title="Add new Product" />
-                  </RequireAuth>
-                }
-              />
+
+          {/* Protected Routes */}
+          <Route element={<RequireAuth />}>
+            <Route path="/" element={<Home />}>
+              <Route path="users">
+                <Route index element={<List />} />
+                <Route path=":userId" element={<Single />} />
+                <Route
+                  path="new"
+                  element={<New inputs={userInputs} title="Add new User" />}
+                />
+              </Route>
+              <Route path="products">
+                <Route index element={<List />} />
+                <Route path=":productId" element={<Single />} />
+                <Route
+                  path="new"
+                  element={<New inputs={productInputs} title="Add new Product" />}
+                />
+              </Route>
             </Route>
           </Route>
         </Routes>
