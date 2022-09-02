b741480699731d3cc496b47efff383338ae5af03
Herby <herbyosbourne31@gmail.com>
2022-09-02 18:05:52 -0500

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import New from './pages/new/New';
 import List from './pages/list/List';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Single from './pages/single/Single';
+import { AuthContext } from './context/AuthContext';
 import { userInputs, productInputs } from './data/formfieldData';
 import CheckUserSession from './components/auth/checkUserSession';
-// import RequireAuth from './components/auth/RequireAuth';
 
 import './theme/dark.scss';
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
           {/* Public Routes */}
-          <Route path="login" element={<Login />} />
+          <Route
+            path="login"
+            element={currentUser ? <Navigate to="/" replace={true} /> : <Login />}
+          />
 
           {/* Protected Routes */}
-          {/* <Route element={<RequireAuth />}> */}
           <Route element={<CheckUserSession />}>
             <Route path="/" element={<Home />}>
               <Route path="users">
@@ -40,8 +44,6 @@ function App() {
               </Route>
             </Route>
           </Route>
-
-          {/* </Route> */}
         </Routes>
       </BrowserRouter>
     </div>
