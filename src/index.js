f16b6931f05a64c983749c2bf1c6f13f13e8630f
Herby <herbyosbourne31@gmail.com>
2022-09-02 18:04:10 -0500

    use Context Provider to Store userSate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import AuthContextProvider from './context/AuthContext2';
+import AuthContextProvider from './context/AuthContext';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
